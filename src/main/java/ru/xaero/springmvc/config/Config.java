fcde35a9868cad99b4bcc3b8c08ae00a19dbd401
0123456Seven <mefefef@bk.ru>
2024-02-06 22:28:33 +0500

    Initial commit

---
+++ src/main/java/ru/xaero/springmvc/config/Config.java
@@ -0,0 +1,9 @@
+package ru.xaero.springmvc.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "ru.xaero.springmvc")
+public class Config {
+}
